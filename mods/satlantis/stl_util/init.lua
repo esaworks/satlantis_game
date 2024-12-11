549eaa83e09e0c2a8f4a83267a43cd3e360b0fb0
esaworks <154466477+esaworks@users.noreply.github.com>
2024-12-11 22:45:20 +0300

    table key comparison utility

--- mods/satlantis/stl_util/init.lua
+++ mods/satlantis/stl_util/init.lua
@@ -1,1 +1,31 @@
 
+
+function stl_dump_table_compare_keys(tableA, tableB, compTag, predicate, msg) 
+	minetest.debug(compTag..": table comparison started.")
+	local keys = {}
+	for k,v in pairs(tableA) do
+		if predicate(k, tableB) then keys[k] = 1 end
+	end
+	for k,v in pairs(tableB) do
+		if predicate(k, tableA) then keys[k] = 1 end
+	end
+	--print filtered results
+	for k,v in pairs(table_name) do
+		minetest.debug(keys[i])
+	end
+	minetest.debug(compTag..": table comparison ended.")
+end
+
+function stl_dump_table_diff(tableA, tableB, compTag) 
+	local has_key = function(t, k)
+		return t[k] == nil
+	end
+	stl_dump_table_compare_keys(tableA, tableB, compTag, has_key, "") 
+end
+
+function stl_dump_table_cross(tableA, tableB, compTag) 
+	local has_key = function(t, k)
+		return t[k] ~= nil
+	end
+	stl_dump_table_compare_keys(tableA, tableB, compTag, has_key, "") 
+end
